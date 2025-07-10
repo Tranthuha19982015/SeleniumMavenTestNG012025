30329634cca6db5745249b4722defaf742f9badd
hoahong19992017 <hoahong19992017@gmail.com>
2025-07-10 18:09:57 +0700

    update code bài 11

--- src/test/java/Bai11_Assert/TestHardAssert.java
+++ src/test/java/Bai11_Assert/TestHardAssert.java
@@ -20,25 +20,38 @@ public class TestHardAssert extends BaseTest {
 
         System.out.println("*** Checking For The Title ***");
 
-        Assert.assertEquals(actualTitle, expectedTitle);
+        Assert.assertEquals(actualTitle, expectedTitle, "Tiêu đề trang không khớp với mong đợi");
     }
 
     @Test(priority = 2)
-    public void testLoginCRMSuccess() {
-        System.out.println("Đăng nhập thành công vào CRM");
+    public void testLoginCRMSuccess() throws InterruptedException {
+        System.out.println("testLoginSuccess");
         // truy cập link web CRM
         driver.get(LocatorsCRM.url);
+        Thread.sleep(1000);
 
         // Login CRM
+        driver.findElement(By.xpath(LocatorsCRM.inputEmail)).clear();
         driver.findElement(By.xpath(LocatorsCRM.inputEmail)).sendKeys("admin@example.com");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(LocatorsCRM.inputPassword)).clear();
         driver.findElement(By.xpath(LocatorsCRM.inputPassword)).sendKeys("123456");
+        Thread.sleep(1000);
         driver.findElement(By.xpath(LocatorsCRM.buttonLogin)).click();
+        Thread.sleep(1000);
 
-//        boolean checkLoginSuccess = driver.findElement(By.xpath(LocatorsCRM.linkMenuCustomer)).isDisplayed();
+        // verify login success
 
+//        code sai - Java sẽ hiện lỗi, không hiển thị lỗi chủ động từ Assert do tester set
+//        boolean checkMenuDashboard = driver.findElement(By.xpath(LocatorsCRM.linkMenuDashboard)).isDisplayed();  // do check bằng isDisplayed()
+//        Assert.assertTrue(checkMenuDashboard, "Menu Dashboard không hiển thị, đăng nhập thất bại");
+
+//        code đúng - để hiển thị lỗi chủ động từ Assert do tester set
         List<WebElement> checkDashboard = driver.findElements(By.xpath(LocatorsCRM.linkMenuDashboard));
         System.out.println("checkDashboard = " + checkDashboard.size());
 
         Assert.assertTrue(checkDashboard.size() > 0, "Menu Dashboard không hiển thị, đăng nhập thất bại");
+
+        System.out.println("Đăng nhập thành công vào CRM");
     }
 }
