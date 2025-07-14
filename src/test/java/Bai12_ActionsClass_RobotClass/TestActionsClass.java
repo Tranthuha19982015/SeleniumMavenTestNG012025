dbaa03cc01e3b6f5c831d235f60d2cac97420a29
hoahong19992017 <73157358+Tranthuha19982015@users.noreply.github.com>
2025-07-14 17:58:49 +0700

    update bài 12

--- src/test/java/Bai12_ActionsClass_RobotClass/TestActionsClass.java
+++ src/test/java/Bai12_ActionsClass_RobotClass/TestActionsClass.java
@@ -96,4 +96,98 @@ public class TestActionsClass extends BaseTest {
         action.contextClick(element).perform();
         Thread.sleep(2000);
     }
+
+    @Test
+    public void moveToElement() throws InterruptedException {
+        driver.get("https://anhtester.com/");
+        Thread.sleep(2000);
+        WebElement element = driver.findElement(By.xpath("//h2[contains(text(),'Kiến thức Automation Testing')]"));
+
+        Actions action = new Actions(driver);
+
+        //Move to element (di chuyển tới title Kiến thức Automation Testing)
+        action.moveToElement(element).perform();
+
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void demoDragAndDropWithActionClass() throws InterruptedException {
+        driver.get("https://www.lambdatest.com/selenium-playground/drag-and-drop-demo");
+        Thread.sleep(1000);
+
+        // Bắt element cần kéo
+        WebElement elementFrom1 = driver.findElement(By.xpath("//span[normalize-space()='Draggable 1']"));
+        WebElement elementFrom2 = driver.findElement(By.xpath("//span[normalize-space()='Draggable 2']"));
+        // Bắt element cần thả đến
+        WebElement elementTo = driver.findElement(By.xpath("//div[@id='mydropzone']"));
+
+        Thread.sleep(1000);
+        Actions action = new Actions(driver);
+        // Kéo và thả
+        action.dragAndDrop(elementFrom1, elementTo).perform();
+        Thread.sleep(1000);
+        action.dragAndDrop(elementFrom2, elementTo).perform();
+
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void inputTextUppercase() throws InterruptedException {
+        driver.get("https://anhtester.com/");
+        Thread.sleep(2000);
+        WebElement element = driver.findElement(By.xpath("//input[@placeholder='Tìm kiếm khóa học . . .']"));
+
+        Actions action = new Actions(driver);
+
+        // Đè giữ phím SHIFT và nhập text -> Chữ in hoa
+        action.keyDown(element, Keys.SHIFT).sendKeys("anh tester").keyUp(Keys.SHIFT).sendKeys(" automation testing").build().perform();
+
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void scrollPageDownAndUp() throws InterruptedException {
+        driver.get("https://anhtester.com/");
+        Actions action = new Actions(driver);
+
+        Thread.sleep(1000);
+        // Scroll down
+        action.keyDown(Keys.CONTROL).keyDown(Keys.END).build().perform();
+        action.keyUp(Keys.CONTROL).keyUp(Keys.END).build().perform();
+        Thread.sleep(2000);
+        // Scroll up
+        action.keyDown(Keys.HOME).perform();
+        action.keyUp(Keys.HOME).perform();
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void copyAndPaste() throws InterruptedException {
+        driver.get("https://anhtester.com/blogs");
+        Thread.sleep(2000);
+
+        WebElement inputCourseElement = driver.findElement(By.xpath("//input[contains(@placeholder, 'Tìm kiếm khóa học')]"));
+        WebElement inputBlogElement = driver.findElement(By.xpath("//input[contains(@placeholder, 'Tìm kiếm bài viết')]"));
+
+        Actions action = new Actions(driver);
+
+        Thread.sleep(1000);
+        // Nhập text vào ô search course
+        inputCourseElement.sendKeys("Selenium");
+        Thread.sleep(1000);
+        //Ctrl + a để bôi đen
+        action.keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).build().perform();
+        Thread.sleep(1000);
+        //Ctrl + c để copy
+        action.keyDown(Keys.CONTROL).sendKeys("x").keyUp(Keys.CONTROL).build().perform();
+        Thread.sleep(1000);
+        //click vào ô Blog search
+        inputBlogElement.click();
+        Thread.sleep(1000);
+        //Ctrl + v để dán
+        action.keyDown(Keys.CONTROL).sendKeys("v").keyUp(Keys.CONTROL).build().perform();
+
+        Thread.sleep(2000);
+    }
 }
